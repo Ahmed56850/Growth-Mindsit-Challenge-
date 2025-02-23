3db772d17d38d8c3ee474b7a9d9907d6f21cc118
Ahmed56850 <ahmedchoru1234@gmail.com>
2025-02-23 22:56:04 +0500

    app.py

--- app.py
+++ app.py
@@ -1,46 +1,46 @@
- #streamlit 
+# streamlit 
 import streamlit as st 
- 
-st.set_page_config(page_title= "Growth Mindset Challenge" , project_page_icon = "⭐")
+
+st.set_page_config(page_title="Growth Mindset Challenge", page_icon="⭐")
 st.title("Growth Mindset Challenge: Web App With Streamlit")    
 
-st.header("Well To My Growth Journey")
+st.header("Welcome To My Growth Journey")
 st.write("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Donec vel metus at justo fermentum tincidunt.")
 
-#quote section 
-st.header("Today Growth Mindset Challenge")
-st.write("LIFE IS THE NAME OF LEARING ")
+# Quote section 
+st.header("Today's Growth Mindset Challenge")
+st.write("LIFE IS THE NAME OF LEARNING ")
 
 st.header("What's Your Challenge Today?")
-user_input = st.text_input("Decribe a Challenge You're Facing; ")
+user_input = st.text_input("Describe a Challenge You're Facing:")
 
-#condition 
+# Condition 
 if user_input:
-    st.success(f"You're Facing: {user_input}. Keep Forwar Towords Your Goal")
+    st.success(f"You're Facing: {user_input}. Keep Moving Forward Towards Your Goal!")
 else:
-    st.warning("Tell As Your Challenge To Get Started!")
+    st.warning("Tell us your challenge to get started!")
 
-#reflexing
+# Reflecting
 st.header("Reflect On Your Learning")
-reflection = st.text_area("Type Your Reflectiions Here:")
+reflection = st.text_area("Type Your Reflections Here:")
 
 if reflection:
-    st.success(f"Great Insight Your Reflection: {reflection}")
+    st.success(f"Great Insight! Your Reflection: {reflection}")
 else:
-    st.info("Reflecting On Past Experience Help You Grow! Share Your Defeculties ")    
+    st.info("Reflecting on past experiences helps you grow! Share your difficulties.")    
 
-#acheivements 
-st.header(f"🎉🏆Celebrate Your Wins!")
-acheivment = st.text_input("Share Somting You've Recently Accomplished:")
+# Achievements 
+st.header("🎉🏆 Celebrate Your Wins!")
+achievement = st.text_input("Share Something You've Recently Accomplished:")
 
-if acheivment:
-    st.success(f"🚀Amazing! You Acheived: {acheivment}")
+if achievement:
+    st.success(f"🚀 Amazing! You Achieved: {achievement}")
 else:
-    st.info("Big Or Small , Every Achievment Counts! Share One Now 😍")    
+    st.info("Big or small, every achievement counts! Share one now 😍")    
 
-#footer 
-st.write("- - -")
-st.write("🚀Keep Beleving In Your Self .Growth Is A Journey, Not A Destination! 🌟")
+# Footer 
+st.write("---")
+st.write("🚀 Keep believing in yourself. Growth is a journey, not a destination! 🌟")
 st.write("** ✍ Created By Ahmed Choru**")
 
 
